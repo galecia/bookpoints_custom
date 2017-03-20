3a58734fc13f7c50c33db48e7cf0c028e092fbee
root <root@sandbox.readingbydesign>
2017-03-20 15:17:33 -0500

    updates to reader dashboard panels and star ratings

--- template.php
+++ template.php
@@ -193,7 +193,8 @@ function bookpoints_custom_radio_before_element($variables) {
   // $element['#attributes']= array("test");
   $checkbox .= drupal_attributes($element['#attributes']) . ' />';
   // $checkbox .= '</div></div>';
-  unset($element['#title']);
+  $checkbox .= "<label class=\"stars\" for=\"" . $element['#title']. "\">" . $element['#title']. "</label>";
+  //unset($element['#title']);
   return $checkbox;
 }
 function bookpoints_custom_radios_before_element($variables){
@@ -201,7 +202,7 @@ function bookpoints_custom_radios_before_element($variables){
   $element = $variables['element'];
    $attributes = array();
    if (isset($element['#id'])) {
-     $attributes['id'] = $element['#id'];
+     //$attributes['id'] = $element['#id'];
    }
    $attributes['class'][] = 'form-radios col-xs-12';
    if (!empty($element['#attributes']['class'])) {
